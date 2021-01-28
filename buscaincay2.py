40a9f9c58d8990f6101c94148a07573fab6de43b
AlfilAlex <cabimax@outlook.com>
2021-01-28 11:10:03 -0600

    Ya es clase pero presenta muchos fallos, sin embargo, ya son menores. El principal problema es que hay funciones sin self que esta siendo llamadas

--- buscaincay2.py
+++ buscaincay2.py
@@ -7,40 +7,46 @@ from tkinter import Tk
 from tkinter.filedialog import askdirectory, askopenfilename
 
 #Creo clase y en init deben estar mypath, filenames y df
-class iniciador():
-    def __init__():
+class incay_facturas():
+    def __init__(self):
         self.mypath = None
         self.filenames = None
         self.df_interes = None
-
+        self.nombre_e = 'None'
+        self.hiperlins = []
+        self.folder_newname = 'None'
         #Aun estoy viendo qué onda
-        nombre_final = nombre_e + '_hiperb.xlsx'
-        folder_newname = 'nombre_e' + '_renombrados'
-        hiperlins = []
+        #nombre_final = self.nombre_e + '_hiperb.xlsx'
+        #folder_newname = self.nombre_e + '_renombrados'
+        
 
-    def mypath():
+    def mypath_func(self):
         Tk().withdraw() # que no aparezca la ventana
-        mypath = askdirectory() # regresa la ruta
-        (_, _, filenames) = next(walk(mypath))             #Se obtiene le nombre de cualquier archivo o carpeta
+        path = askdirectory() # regresa la ruta
+        (_, _, filenames) = next(walk(path))             #Se obtiene le nombre de cualquier archivo o carpeta
         filenames = [x.split('.')[0] for x in filenames if '.pdf' in x or '.PDF' in x]        #Seleccion de pdfs
-        self.mypath = mypath
+        self.mypath = path
         self.filenames = filenames
-        nombre_e = str(mypath.split('/')[-1])
+        self.nombre_e = str(self.mypath.split('/')[-1])
+        self.folder_newname = self.nombre_e + '_renombrados'
 
-    def dataframe():
+    def dataframe(self):
         ruta_excel_original = askopenfilename() 
         df_interes = pd.read_excel(ruta_excel_original, 
-                                    sheet_name = nombre_e, 
+                                    sheet_name = self.nombre_e, #Si primero busca el excel, saltará un error
                                     header=5, 
                                     usecols='A:F',
                                     index_col=0).dropna(axis=0, how='all')
         self.df_interes = df_interes
+
+    def iniciador(self):
+        self.comparador(self.df_interes, self.filenames, self.mypath)
         
 
     #Nombre de la carpeta = Destino
     #folder_newname = 'nombre_e' + '_renombrados'
-    def crear_carpeta(ruta):
-        ruta_nueva = os.path.join(ruta, folder_newname)
+    def crear_carpeta(self, ruta):
+        ruta_nueva = os.path.join(ruta, self.folder_newname)
         os.mkdir(ruta_nueva)
         #print('Se creo una nueva carpeta destino')
 
@@ -66,19 +72,19 @@ class iniciador():
     #hiperlins = []
     def nuevacolumna(path, elemento, esta=True):
         if esta:
-            hiperlins.append('=HYPERLINK("'+path+'","'+str(elemento)+'")')
+            self.hiperlins.append('=HYPERLINK("'+path+'","'+str(elemento)+'")')
         else:
-            hiperlins.append(elemento)
+            self.hiperlins.append(elemento)
             
     #Recorre y compara los nombres de los excel y los nombres de los pdf en la carpeta
-    def iniciador(df_interes, filenames, mypath):
+    def comparador(self, df_interes, filenames, mypath):
         i = 0
         control = 0
         for elemento in df_interes['FACTURA']: #df_interes['FACTURA'] tiene lo del excel
             bandera = False
 
             if i ==0:
-                crear_carpeta(mypath)
+                self.crear_carpeta(mypath)
 
             if i % int(len(df_interes)/100) == 0:
                 os.system('clear')
@@ -94,15 +100,15 @@ class iniciador():
                 
                 if re.findall(string.replace('-', ''), str(elemento).replace('-', '')):            #Elemento: del excel     #archivo: de la carpeta
                     i +=1            
-                    cambiarnombre(i, str(elemento), archivo, mypath)
+                    self.cambiarnombre(i, str(elemento), archivo, mypath)
                     bandera = True
                     print(archivo, elemento)
                     break
                     
             if bandera == False:
                 i +=1
-                cambiarnombre('nohay', elemento, archivo, mypath, esta=False)
-            print(i, elemento)
+                self.cambiarnombre('nohay', elemento, archivo, mypath, esta=False)
+            #print(i, elemento)
 
 #iniciador(df_interes, filenames, mypath)
 #nombre_final = nombre_e + '_hiperb.xlsx'
--- intertez_incay.py
+++ intertez_incay.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from buscaincay2 import incay_facturas
 #from buscaincay2 import carpeta_madre
 
 
@@ -6,6 +7,16 @@ from tkinter import *
 root = Tk()
 root.geometry('400x300')
 
+facturas = incay_facturas()
+
+
+
+def buscacarpeta():
+    facturas.mypath_func()
+def buscaexcel():
+    facturas.dataframe()
+def inicia_programa():
+    facturas.iniciador()    
 
 
 
@@ -13,14 +24,14 @@ frame = Frame(root, width=400, height=250)
 frame.grid(row=0, column=0)
 
 
-boton_carpeta = Button(frame, text='Buscar carpeta')
+boton_carpeta = Button(frame, text='Buscar carpeta', command=buscacarpeta)
 boton_carpeta.grid(row=0, column = 0, padx=20, pady=30)
 
 
-boton_archivo = Button(frame, text='Buscar excel')
+boton_archivo = Button(frame, text='Buscar excel', command=buscaexcel)
 boton_archivo.grid(row=1, column=0, padx=20, pady=30)
 
-boton_iniciar = Button(frame, text='Iniciar proceso')
+boton_iniciar = Button(frame, text='Iniciar proceso', command=inicia_programa)
 boton_iniciar.grid(row=2, column=0, pady=30, columnspan=2)
 
 
